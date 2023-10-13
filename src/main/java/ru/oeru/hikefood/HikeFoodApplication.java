14a0c5fbcc73e309969ad4ce87090dbaacd85eb7
Александров Сергей <AleksandrovSA.dev@gmail.com>
2023-10-13 14:15:06 +0300

    Init commit

---
+++ src/main/java/ru/oeru/hikefood/HikeFoodApplication.java
@@ -0,0 +1,13 @@
+package ru.oeru.hikefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/oeru/hikefood/HikeFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oeru.hikefood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikeFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
